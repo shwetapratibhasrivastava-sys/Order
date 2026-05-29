0e0997ba74ff8f8d8004da050bfeb9abee2a111b
Shweta Srivastava <shwetapratibhasrivastava@gmail.com>
2026-05-29 15:54:21 +0530

    updated

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -6,13 +6,13 @@ import {
   getOrderById,
   updateOrder,
 } from "../controllers/orderController.js";
-
+import authMiddleware from "../middleware/authMiddleware.js""
 const orderRoute = express.Router();
 
 orderRoute.post("/", createOrder);
-orderRoute.get("/", getOrder);
-orderRoute.get("/:id", getOrderById);
-orderRoute.put("/:id", updateOrder);
-orderRoute.delete("/:id", deleteOrder);
+orderRoute.get("/", authMiddleware,getOrder);
+orderRoute.get("/:id",authMiddleware, getOrderById);
+orderRoute.put("/:id", authMiddleware,updateOrder);
+orderRoute.delete("/:id", authMiddleware,deleteOrder);
 
 export default orderRoute;
